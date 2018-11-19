f6c3a986e670ca9706c7d0038df364a6275d7480
zyadab <45104462+zyadab@users.noreply.github.com>
2018-11-19 06:54:12 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args){
+    foo();
+    bar();
+  }
+  
+  public void foo(){
+    System.out.println("This is the foo method.");
+  }
+  
+  public void bar(){
+    System.out.println("This is the bar method.");
+  }
+}
