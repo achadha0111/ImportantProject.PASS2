97726c06014e16c7615259614176897668c752a5
Igor Wodiany <igor.wodiany@gmail.com>
2018-11-19 11:51:29 +0000

    Some change

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
   }
   
   public void bar(){
-    System.out.println("This is the bar method.");
+    System.out.println("Branch change! This is the bar method.");
     System.out.println("I did some changes in this method in MergeConflictBranch");
 
   }
