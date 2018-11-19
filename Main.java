7373d4faf914c4c16029044761cb01fed47a81e1
zyadab <45104462+zyadab@users.noreply.github.com>
2018-11-19 06:56:17 +0000

    Update Main.java
    Some changes to "bar" method

--- Main.java
+++ Main.java
@@ -10,5 +10,7 @@ public class Main {
   
   public void bar(){
     System.out.println("This is the bar method.");
+    System.out.println("I did some changes in this method in MergeConflictBranch");
+
   }
 }
